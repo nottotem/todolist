dd35a0336258b9819375b7923c3ac98899d7d40c
nottotem <totemstu@gmail.com>
2023-06-22 16:56:45 -0300

    small style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 function Header() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-20">
-      <h1 className="font-mono text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-slate-100 to-slate-400">
+      <h1 className="font-mono text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-slate-100 to-slate-400 ">
         To Do List
       </h1>
-      <h3 className="font-mono text-xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-slate-100 to-slate-400">
+      <h3 className="font-mono text-xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-slate-100 to-slate-400 ">
         Add all your tasks and don't forget anything!
       </h3>
     </div>
