4fa58b76827685fe7d49c5bfc8fce6319be18eb8
nottotem <totemstu@gmail.com>
2023-06-24 16:51:15 -0300

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -16,19 +16,25 @@ function App() {
   }, [todos]);
 
   return (
-    <div className="flex flex-col items-center min-h-screen w-full gap-4 bg-neutral-950 pb-14">
+    <div className="flex flex-col items-center min-h-screen w-full bg-neutral-950 pb-10">
       <Navbar />
-      <Header />
-      <Form
-        input={input}
-        setInput={setInput}
-        todos={todos}
-        setTodos={setTodos}
-        editTodo={editTodo}
-        setEditTodo={setEditTodo}
-      />
-      <TasksText />
-      <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+      <div className="flex flex-col items-center justify-center mt-14 w-full gap-4">
+        <Header />
+        <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+        />
+        <TasksText />
+        <TodosList
+          todos={todos}
+          setTodos={setTodos}
+          setEditTodo={setEditTodo}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillMail } from "react-icons/ai";
+import { RiUserSmileFill } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <div className="flex flex-row items-center justify-end w-full h-14 border-b border-neutral-900 gap-2 pr-4">
+    <div className="fixed flex flex-row items-center justify-end w-full h-14 border-b border-neutral-900 gap-2 px-8 bg-neutral-950">
+      <div className="flex flex-row items-center h-full w-full gap-2">
+        <p>
+          <RiUserSmileFill className="text-base text-neutral-400" />
+        </p>
+        <p className="font-mono text-transparent bg-clip-text bg-gradient-to-b from-cyan-300 to-cyan-600 font-semibold select-none">
+          nottotem, inc.
+        </p>
+      </div>
       <a
         href="https://www.linkedin.com/in/francisco-toti-1b1354241/"
         target="_blank"
@@ -29,9 +38,6 @@ function Navbar() {
         rel="noopener noreferrer"
       >
         <div className="flex items-center justify-center h-full">
-          {/* <p className="font-mono text-sm text-neutral-500 select-none">
-            Contact me
-          </p> */}
           <AiFillMail className="text-2xl text-neutral-500 hover:text-neutral-400" />
         </div>
       </a>
