f210bd5bf795aea88338a8f2ca74208f9e8dd6a7
nottotem <totemstu@gmail.com>
2023-06-29 21:40:09 -0300

    new ToDoApp component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Form from "./components/Form";
-import TasksText from "./components/TasksText";
-// import PendingTasks from "./components/PendingTasks";
-import TodosList from "./components/TodosList";
+import ToDoApp from "./components/ToDoApp";
 
 function App() {
-  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState(initialState);
-  const [editTodo, setEditTodo] = useState(null);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
   return (
     <div className="flex flex-col items-center min-h-screen w-full bg-neutral-950 pb-10">
       <Navbar />
       <div className="flex flex-col items-center justify-center mt-20 w-full gap-4">
-        <Header />
-        <Form
-          input={input}
-          setInput={setInput}
-          todos={todos}
-          setTodos={setTodos}
-          editTodo={editTodo}
-          setEditTodo={setEditTodo}
-        />
-        <TasksText todos={todos} setTodos={setTodos} />
-        {/* <PendingTasks todos={todos} /> */}
-        <TodosList
-          todos={todos}
-          setTodos={setTodos}
-          setEditTodo={setEditTodo}
-        />
+        <ToDoApp />
       </div>
     </div>
   );
---
+++ src/components/ToDoApp.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import Form from "./Form";
+import TasksText from "./TasksText";
+// import PendingTasks from "./components/PendingTasks";
+import TodosList from "./TodosList";
+
+function ToDoApp() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-4">
+      <Header />
+      <Form
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+      />
+      <TasksText todos={todos} setTodos={setTodos} />
+      {/* <PendingTasks todos={todos} /> */}
+      <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+    </div>
+  );
+}
+
+export default ToDoApp;
